989bbd48ee08326efaba8a357a92edab3a2e79fc
EJSEO <eojinseo77@postech.ac.kr>
2026-08-19 11:21:47 +0000

    GMM_static_POSTECH

--- test2.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-car_2_position = np.array([[25.0], [15.6], [1.8]])
-car_2_pos = car_2_position[:, 0]
-print("car_2_pos:", car_2_pos)
-print("car_2_pos[0]:", car_2_pos[0])
--- tests/test_channel.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestChannel(unittest.TestCase):
-    def test_doppler(self):
-        self.assertTrue(True)
-        
-if __name__ == '__main__':
-    unittest.main()
